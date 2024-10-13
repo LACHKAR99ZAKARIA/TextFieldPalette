29aa650ac84d6d1a18544330171c284a84c38188
Zakaria Lachkar <lachkar99zakaria@gmail.com>
2024-10-13 15:35:50 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TextFieldPalette",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TextFieldPalette",
+            targets: ["TextFieldPalette"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TextFieldPalette"),
+
+    ]
+)
